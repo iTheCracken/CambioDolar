db11ca51f3e02cde38339ea3812bbf64d430f4fd
iTheCracken <jcovarrubiasp_halo3@hotmail.com>
2018-11-07 09:34:15 -0600

    Casi listo

--- conexion.js
+++ conexion.js
@@ -1,42 +1,64 @@
 const sqlite3 = require('sqlite3').verbose();
+var casual = require ('casual');
 
-// open the database
-let db = new sqlite3.Database('./db/cambios.db', (err) => {
+let db = new sqlite3.Database('./db/pruebas.db', (err) => {
     if (err) {
         console.error(err.message);
     }
-    console.log('Conectado a la base de datos Cambios');
+    // console.log('Conectado a la base de datos Pruebas');
 });
 
-let sql = `SELECT valor FROM dolar
-           ORDER BY valor`;
- 
-db.all(sql, [], (err, rows) => {
-  if (err) {
-    throw err;
-  }
-  rows.forEach((row) => {
-    console.log(row.valor);
-  });
-});
+if (count() < 10) {
+    select();
+} else {
+    insertar();
+}
 
 db.close((err) => {
     if (err) {
         console.error(err.message);
     }
-    console.log('Conexión Finalizada');
+    // console.log('Conexión Finalizada');
 });
 
 function select() {
-    let sql = `SELECT valor FROM dolar
-           ORDER BY valor`;
+    let sql = `SELECT * FROM alumnos`;
 
     db.all(sql, [], (err, rows) => {
         if (err) {
             throw err;
         }
         rows.forEach((row) => {
-            console.log(row.name);
+            console.log(row.nombre);
         });
     });
+}
+
+function count() {
+    var x = 1;
+    const sql = `SELECT count(*) as total FROM alumnos`;
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        rows.forEach((row) => {
+            x = row.total;
+            console.log(row.total);
+        });
+    });
+    return x;
+}
+
+function crearTabla() {
+    db.run('CREATE TABLE alumnos(matricula varchar, nombre text, edad int)');
+}
+
+async function insertar() {
+    db.run(`INSERT INTO alumnos(matricula, nombre, edad) VALUES(?,?,?)`, [`150${casual.letter}00${casual.integer(from = 0, to = 99)}`, `${casual.name}`, `${casual.integer(from = 18, to = 30)}`], function (err) {
+        if (err) {
+            return console.log(err.message);
+        }
+        console.log(`A row has been inserted with rowid ${this.lastID}`);
+    });
+    await count();
 }
